43d1ecabcc4f28b1ec9afa3cc45b6bfc2947d5bd
nandocp <fcpontes@gmail.com>
2017-06-29 15:50:45 -0300

    Etch-A-Sketch faltando botão novo layout | Etch-A-Sketch missing only new layout functionality

--- etch-a-sketch/scripts/script.js
+++ etch-a-sketch/scripts/script.js
@@ -1,37 +1,40 @@
 $(document).ready(function() {
 	var newCol = '<div class="col"></div>';
 	var newBlock = '<div class="square-block"></div>'
+	var layoutGrid = 64;
 
-	for (i = 1; i <= 32; i++) {
+	/* $('#newlayout-button').on('click', '.button-new' function() {
+		$('.box-number').on('keyup', function() {
+			layoutGrid = +$(this).val();
+			return layoutGrid;
+			location.reload();
+		});
+	}); */
+
+	for (i = 1; i <= layoutGrid; i++) {
 		$('#container').append(newCol);
 	}
-	for (i = 1; i <= 32; i++) {
+	for (i = 1; i <= layoutGrid; i++) {
 		$('.col').append(newBlock);
 	}
 
 	$('.col').on('mouseenter', '.square-block', function() {
-		var color = Math.floor((Math.random() - 0.3)*10);
-		switch (color) {		
-		case 1:
-			$(this).addClass('hover1');
-			break;
-		case 2:
-			$(this).addClass('hover2');
-			break;
-		case 3:
-			$(this).addClass('hover3');
-			break;
-		case 4:
-			$(this).addClass('hover4');
-			break;
-		case 5:
-			$(this).addClass('hover5');
-			break;
-		case 6:
-			$(this).addClass('hover6');
-			break;
-		}
+		var num = Math.floor((Math.random() - 0.3)*10);
+		var paintColors = ['#FF0000', '#FF8000', '#FFFF00', 
+											 '#008000', '#0000FF','#A000C0'];
+		$(this).css("background-color", paintColors[num]);
 	});
+
+	$('#newrefresh').on('mouseenter', '.refresh', function() {
+		var colors = ['#F28560', '#F2B560', '#F2CD60', '#56D88C',
+									'#60C1F2', '#BD93D8', '#FFDCFC', '#C8FEFF', '#FFF'];
+		var rand = Math.floor(Math.random()*colors.length);
+		$(this).css("background-color", colors[rand]);
+	}); 
+	$('#newrefresh').on('mouseleave', '.refresh', function() {
+		$(this).css("background-color", 'buttonface');
+	});
+
 	$('.refresh').click(function() {
 		location.reload();
 	});
