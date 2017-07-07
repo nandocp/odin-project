06a4ef7dbbdbef005a65543e92e8fdcf121fbddc
nandocp <fcpontes@gmail.com>
2017-07-07 14:11:36 -0300

    etch-a-sketc, google-homepage (easy. difficult to be finished), learn-ruby (0, 1, 2, 3, 4)

--- learn-ruby/04_pig_latin/pig_latin.rb
+++ learn-ruby/04_pig_latin/pig_latin.rb
@@ -1,1 +1,29 @@
 #write your code here
+def translate(str)
+  arr_palavras = str.split
+  i = 0
+  j = 0
+  const = "ay"
+  result = []
+
+  while i < arr_palavras.length
+  str_palavra = arr_palavras[i]
+  particula = ""
+    if /[aeiou]/ === str_palavra[0]
+      result << str_palavra + const
+    else
+      if str_palavra[0] === "q" && str_palavra[1] ==="u"
+        particula << str_palavra.slice!(0..1)
+      elsif /[aeiou]/ != str_palavra[0] && /[aeiou]/ === str_palavra[1]
+        particula << str_palavra.slice!(0)
+      elsif /[aeiou]/ != str_palavra[0] && /[aeiou]/ != str_palavra[1] && /[aeiou]/ != str_palavra[2] && /[aeiou]/ === str_palavra[3]
+        particula << str_palavra.slice!(0..2)
+      elsif /[aeiou]/ != str_palavra[0] && /[aeiou]/ != str_palavra[1] && /[aeiou]/ === str_palavra[2]
+        particula << str_palavra.slice!(0..1)
+      end
+      result << str_palavra + particula + const
+    end
+    i += 1
+  end
+  return result.join(" ")
+end
--- learn-ruby/05_book_titles/book.rb
+++ learn-ruby/05_book_titles/book.rb
@@ -1,3 +1,12 @@
 class Book
 # write your code here
+	attr_accessor :title
+
+	def title
+	i = 0
+		while i < @title.length
+			return @title.capitalize
+			i += 1
+		end
+	end
 end
