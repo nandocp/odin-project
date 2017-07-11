62ae6d1e1ed30b066fa08e2d207152d089a28fc1
nandocp <fcpontes@gmail.com>
2017-07-11 08:53:50 -0300

    resposta exercício 05-book-titles | answer to 05-book-titles

--- learn-ruby/05_book_titles/book.rb
+++ learn-ruby/05_book_titles/book.rb
@@ -3,10 +3,20 @@ class Book
 	attr_accessor :title
 
 	def title
-	i = 0
-		while i < @title.length
-			return @title.capitalize
+		tit = @title.split
+		i = 1
+		cap = []
+
+		cap << tit[0].capitalize
+		while i < tit.length
+			if tit[i] === "and" || tit[i] === "in" || tit[i] === "the" || tit[i] === "of" || tit[i] === "a" || tit[i] === "an"
+				cap << tit[i]
+			else
+				cap << tit[i].capitalize
+			end
 			i += 1
 		end
+		return cap.join(" ")
 	end
+
 end
