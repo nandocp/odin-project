95282723c1999676b2671aeb7582a557e90c05b4
nandocp <fcpontes@gmail.com>
2017-07-11 20:25:52 -0300

    adicionada gem sorcery, login/logout | added sorcery gem for login/logout

---
+++ blogger/app/controllers/author_sessions_controller.rb
@@ -0,0 +1,18 @@
+class AuthorSessionsController < ApplicationController
+	def new # responsible for rendering its related form
+	end
+
+	def create # responsible for accepting the submission of the form created
+		if login(params[:email], params[:password])
+			redirect_back_or_to(articles_path, notice: 'Logged in successfully')
+		else
+			flash.now.alert = "Login failed"
+			render action: :new
+		end
+	end
+
+	def destroy # responsible for removing a record of the appropriate type
+		logout
+		redirect_to(:authors, notice: 'Logged out')
+	end
+end
