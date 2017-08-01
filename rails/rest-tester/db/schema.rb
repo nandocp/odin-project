42f1b15240850bfbdabb857acea3f205323a10d6
nandocp <fcpontes@gmail.com>
2017-08-01 13:48:31 -0300

    added validations to User model

--- rails/rest-tester/db/schema.rb
+++ rails/rest-tester/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726014443) do
+ActiveRecord::Schema.define(version: 20170801135921) do
 
   create_table "unicorns", force: :cascade do |t|
     t.string "name"
@@ -25,6 +25,9 @@ ActiveRecord::Schema.define(version: 20170726014443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "gender"
+    t.string "email"
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
