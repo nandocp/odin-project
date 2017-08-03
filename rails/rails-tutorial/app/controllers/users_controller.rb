147aa1d395720a1d04333edee2c56e2304b303af
nandocp <fcpontes@gmail.com>
2017-08-03 14:10:28 -0300

    implement basic login/logout

--- rails/rails-tutorial/app/controllers/users_controller.rb
+++ rails/rails-tutorial/app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+      log_in @user
   		flash[:success] = "Welcome to the Rails Tutorial App!"
   		redirect_to user_url(@user) # or just redirect_to @user
   	else
--- rails/rails-tutorial/config/routes.rb
+++ rails/rails-tutorial/config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
 	root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new' # page for a new session
+  post '/login', to: 'sessions#create' # create a new session
+  delete '/logout', to: 'sessions#destroy' # delete a session
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
